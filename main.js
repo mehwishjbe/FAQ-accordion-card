28d9d3f03964a8ded20f132df2bfe17041b3a3b8
mehwishjbe <mehwish.jbe@gmail.com>
2023-09-19 13:06:10 +0200

    toogle on arrow is working

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const boxes = document.querySelectorAll(".box");
 
 boxes.forEach(element => {
+  const icon = element.querySelector('.arrow');
   element.addEventListener('click', () => {
-    console.log(element);
+    icon.classList.toggle('active');
   });
 });
