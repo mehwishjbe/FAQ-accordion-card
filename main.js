2f4d0a40b17198b5fdddad33036941b8f31f0806
mehwishjbe <mehwish.jbe@gmail.com>
2023-09-19 13:07:13 +0200

    toggle on answers added

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const boxes = document.querySelectorAll(".box");
 
 boxes.forEach(element => {
   const icon = element.querySelector('.arrow');
+  const answer = element.querySelector('.answer');
   element.addEventListener('click', () => {
     icon.classList.toggle('active');
+    answer.classList.toggle('active');
   });
 });
