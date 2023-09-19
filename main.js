edddd2a127c29f701c4eaeb5881193f9a1a0dc58
mehwishjbe <mehwish.jbe@gmail.com>
2023-09-19 13:09:12 +0200

    font bold on hover working

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ const boxes = document.querySelectorAll(".box");
 boxes.forEach(element => {
   const icon = element.querySelector('.arrow');
   const answer = element.querySelector('.answer');
+  const spantext = element.querySelector('span');
+
   element.addEventListener('click', () => {
     icon.classList.toggle('active');
     answer.classList.toggle('active');
+    spantext.classList.toggle('active');
   });
 });
